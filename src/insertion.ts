9febd8b2cdda6af6580c5743b0294b347be341d0
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2023-07-18 13:44:58 +0200

    Fix bug

--- src/insertion.ts
+++ src/insertion.ts
@@ -15,10 +15,10 @@ export function sortAsc(arr: Array<number>): Array<number> {
 
 // It starts from second element of the array and swaps smaller numbers to the left until it finds a number less than itself
 export function sortDesc(arr: Array<number>): Array<number> {
-    for (let i = 1; i < arr.length - 1; i++) {
+    for (let i = 1; i <= arr.length - 1; i++) {
         let j = i;
-        while((j > 0) && (arr[j] < arr[j-1])) {
-            arr = swap(arr, j, j + 1);
+        while((j > 0) && (arr[j] > arr[j-1])) { // j: am I greater than my left mate? If yes, take me as close as possible to the beginning of the array until I find a mate greater than me. If so, leave me there and continue with te mate that was to my right originally
+            arr = swap(arr, j, j - 1);
             j--;
         }
     }
