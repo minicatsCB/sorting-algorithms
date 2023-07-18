27e159630398a8037fec36144e18562b20c513ab
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2023-07-18 13:04:28 +0200

    Implement bubble algorithm

---
+++ bubble.ts
@@ -0,0 +1,30 @@
+import { swap } from "./common";
+
+// Starts from the beginning of the array, checks each pair of numbers and swap them if the first is less than the second.
+function sortAsc(arr: Array<number>): Array<number> {
+    for(let lap = 0; lap < arr.length - 1; lap++) {
+        for(let i = 0; i < arr.length - 1; i++) {
+            if(arr[i] > arr[i + 1]) {
+                arr = swap(arr, i, i + 1);
+            }
+        }
+    }
+
+    return arr;
+}
+
+function sortDesc(arr: Array<number>): Array<number> {
+    for(let lap = 0; lap < arr.length - 1; lap++) {
+        for(let i = arr.length - 1; i > 0; i--) {
+            if(arr[i] > arr[i - 1]) {
+                arr = swap(arr, i, i - 1);
+            }
+        }
+    }
+
+    return arr;
+}
+
+let arr = [9, 1, 8, 2, 7, 3];
+let res = sortDesc(arr);
+console.log(res);
